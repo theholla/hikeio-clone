1ae1690626879524d80cdc8ecc9921145ed5fff0
aimee reiss & diana holland <student@epicodus.com>
2015-11-12 10:10:41 -0800

    Install butterknife for better resource binding

--- app/src/main/java/com/example/guest/hikeioclone/MainActivity.java
+++ app/src/main/java/com/example/guest/hikeioclone/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.guest.hikeioclone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.btnMap) Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
